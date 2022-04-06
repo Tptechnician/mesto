f1936ae5e255b6d3aa685c361b5f1f718134db00
Pavel Demin <deminps.2008@gmail.com>
2022-04-07 00:50:56 +0500

    feat: сделан popup для добавления картинок

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 // код для открытия закрытия popup
 let profileEditButton = document.querySelector('.profile__edit-button');
+let profileAddButton = document.querySelector('.profile__add-button');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
+let popupCloseAddImage = document.querySelector('#button-close-add-image');
+let popupAddImage = document.querySelector('#add-image');
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
@@ -16,8 +19,14 @@ function togglePopup() {
 }
 
 profileEditButton.addEventListener('click', togglePopup);
-
 popupClose.addEventListener('click', togglePopup);
+
+function togglePopupAddImage() {
+  popupAddImage.classList.toggle('popup_opened');
+}
+
+profileAddButton.addEventListener('click', togglePopupAddImage);
+popupCloseAddImage.addEventListener('click', togglePopupAddImage);
 
 
 /* Код для закрытия popup при клике по overlay
@@ -50,3 +59,48 @@ function formSubmitHandler (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 form.addEventListener('submit', formSubmitHandler);
+
+let formAddImage = document.forms.formpopupaddimage;
+let titleInput = form.elements.inputtitle;
+let linkInput = form.elements.inputlink;
+let elementDescription = document.querySelector('.element__description');
+let elementImage = document.querySelector('.element__image');
+
+function formSubmitAddImage (evt) {
+  evt.preventDefault();
+
+  elementImage.textContent = linkInput.value;
+  elementDescription.textContent = titleInput.value;
+
+  togglePopupAddImage();
+}
+
+formAddImage.addEventListener('submit', formSubmitAddImage);
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
