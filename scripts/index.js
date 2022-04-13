a768bbe65b28abe681406204dc6926171743e041
Pavel Demin <deminps.2008@gmail.com>
2022-04-13 11:41:06 +0500

    feat: Плавное закрытие popupviewimage

--- scripts/index.js
+++ scripts/index.js
@@ -36,6 +36,7 @@ let profileAddButton = document.querySelector('.profile__add-button');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
 let popupCloseAddImage = document.querySelector('#button-close-add-image');
+
 let popupAddImage = document.querySelector('#add-image');
 let form = document.forms.formpopup;
 let nameInput = form.elements.inputname;
@@ -45,7 +46,7 @@ let profileActivity = document.querySelector('.profile__activity');
 let popupViewImage = document.querySelector('.popup__view-image');
 
 
-const popupCloseViewImage = document.querySelector('.button-close-view-image');
+
 
 function addCards() {
   const cards = initialCards.map(getElement);
@@ -60,34 +61,33 @@ function getElement(element) {
   const image = getElementTemplate.querySelector('.element__image');
   const deleteButton = getElementTemplate.querySelector('.element__delete-button');
   const buttonLike = getElementTemplate.querySelector('.element__like-button');
-  
-  
+    
   description.textContent = element.name;
   image.src = element.link;
-  
+  image.alt = element.name;
+
   formAddImage.addEventListener('submit', formSubmitAddImage);
   deleteButton.addEventListener('click', removeCard);
   buttonLike.addEventListener('click', like);
-  image.addEventListener('click', togglePopupViewImage);
-  description.addEventListener('click', togglePopupViewImage);
-  return getElementTemplate;
+  image.addEventListener('click', () => togglePopupViewImage(element));
   
+  return getElementTemplate;
 }
-
 let popupImage = document.querySelector('.popup__image');
 let popupTitleViewImage = document.querySelector('.popup__title__view-image');
-const description = document.querySelector('.element__description');
-const image = document.querySelector('.element__image');
+const popupCloseViewImage = document.querySelector('.button-close-view-image');
 
-function togglePopupViewImage (evt) {
+function togglePopupViewImage (element) {
   popupViewImage.classList.toggle('popup_opened');
-  const src = this.currentSrc;
-  popupImage.src = src;
-  popupTitleViewImage.textContent = description.textContent;
-  
+  if (popupViewImage.classList.contains('popup_opened') === true){
+    popupImage.src = element.link;
+    popupTitleViewImage.textContent = element.name;
+    popupImage.alt = element.name
+  }
 }
 
 popupCloseViewImage.addEventListener('click', togglePopupViewImage);
+
 
 function formSubmitAddImage (evt) {
   evt.preventDefault();
@@ -102,6 +102,8 @@ function formSubmitAddImage (evt) {
 
   titleInput.value = '';
   linkInput.value = '';
+
+  
 }
 
 
